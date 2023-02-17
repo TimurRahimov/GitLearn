ffd271954aa7b6d9107343ccd3cfa88b5b04244d
TimurRahimov <lollipopnote8@gmail.com>
2023-02-17 21:46:43 +0300

    Third commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+# Comment 1
+# Comment 2
+# Comment 3
+
 def my_function(first, second):
-    return first * second 10
+    return first + second
     
 def another_function(first):
     return first + 1
